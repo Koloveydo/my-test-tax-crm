ae5297306fef2be73124962facdbec185a2bb736
Vladyslav <vladuslavkondrun12@gmail.com>
2024-09-29 18:08:39 +0300

    Виправлено помилки

--- docs/v1/app/app.py
+++ docs/v1/app/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, jsonify, redirect, flash, session
-
+from flask import Flask, render_template, jsonify, redirect, flash, sessio
 app = Flask(__name__)
 app.secret_key = 'Asdasd@E!d121'
 
@@ -13,6 +12,9 @@ def componentsView():
 def baseView():
     return render_template('pages/base.html')
 
+# @app.route('/plus_button')
+# def plus_buttonView():
+#     return render_template('components/plus_button.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
