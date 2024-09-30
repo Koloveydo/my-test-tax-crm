d7fcef6f6e32874918c6248524df789f208a6d39
GoldSatan <vadim.romanyuk.2003@gmail.com>
2024-09-30 19:49:20 +0300

    Revert "Виправлено помилки"
    This reverts commit ae5297306fef2be73124962facdbec185a2bb736.

--- docs/v1/app/app.py
+++ docs/v1/app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, jsonify, redirect, flash, sessio
+from flask import Flask, render_template, jsonify, redirect, flash, session
+
 app = Flask(__name__)
 app.secret_key = 'Asdasd@E!d121'
 
@@ -12,9 +13,6 @@ def componentsView():
 def baseView():
     return render_template('pages/base.html')
 
-# @app.route('/plus_button')
-# def plus_buttonView():
-#     return render_template('components/plus_button.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host='0.0.0.0')
