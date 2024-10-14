6226a5fe5786e546e0aefd2c49b5da6db3c03ce1
Koloveydo <107243603+Koloveydo@users.noreply.github.com>
2024-10-14 21:17:14 +0300

    added dark-theme

--- docs/v1/app/models.py
+++ docs/v1/app/models.py
@@ -37,7 +37,6 @@ class UserDetails(db.Model):
 class Lead(db.Model):
     __tablename__ = 'lead'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(40))
     lead_details = db.Column(db.Integer, db.ForeignKey('lead_details.id'), nullable=False)
     datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow, nullable=False) 
     datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
