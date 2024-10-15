29fcb1b0c204cbb92688e852bac874d94ecb177e
Vladyslav <vladuslavkondrun12@gmail.com>
2024-10-15 13:34:27 +0300

    Виправлено помилки

--- docs/v1/app/app.py
+++ docs/v1/app/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, jsonify, redirect, flash, session
-from flask_sqlalchemy import SQLAlchemy 
-from models import db, User, UserDetails, Lead, LeadDetails, Activity, ActivityDetails, Communication, CommunicationDetails, Skill, SkillDetails, Tag, TagDetails
+from models import *
 
 app = Flask(__name__)
+username='root'
+password='0000'
+host='localhost'
+port='3306'
+database='tax_crm'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
 app.secret_key = 'Asdasd@E!d121'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:0000@localhost:3306/tax_crm'
 db.init_app(app)
-
-with app.app_context():
-    db.create_all()
 
 @app.route('/')
 @app.route('/components')
@@ -20,20 +23,16 @@ def components_view():
 def base_view():
     return render_template('pages/base.html')
 
-@app.route('/create-user')
-def create_user_view():
-    ### create all for user model and user model
-    ### print some informations of user
-    
-    ### create all for Lead model and Lead model
-    ### print some informations of Lead
-    
-    ### ...
-    ### ...
-    
-    
-    return render_template('pages/components.html')
+@app.route('/users')
+def users_view():
+    users = UserDetails.query.all()
+    return render_template('pages/database_testing.html', users=users)
 
+
+'''@app.route('/create-lead')
+def create_lead_view():
+     leads = UserDetails.query.all()
+     return render_template('pages/Checking_Database.html', leads=leads)'''
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')  
--- docs/v1/app/models.py
+++ docs/v1/app/models.py
@@ -1,50 +1,39 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:0000@localhost:3306/tax_crm'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class User(db.Model): 
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    user_basic_details = db.Column(db.Integer, db.ForeignKey('user_details.id'), nullable=False)
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)  
-    user_details = db.relationship('UserDetails', backref='user')
-    
-    def __repr__(self):
-        return f"<User {self.id}, {self.user_basic_details} >"
+
+    id                 = db.Column(db.Integer, primary_key=True)
+    user_basic_details = db.Column(db.Integer, db.ForeignKey('user_details.id'))
+    datetime_update    = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create    = db.Column(db.DateTime, default=datetime.utcnow)  
    
 class UserDetails(db.Model):
     __tablename__ = 'user_details'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(40))
-    last_name = db.Column(db.String(40))
-    email = db.Column(db.String(40), unique=True)
-    job_title = db.Column(db.String(60))
-    phone_number = db.Column(db.String(14))
-    address = db.Column(db.String(50))
-    url_image = db.Column(db.Text)
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __repr__(self):
-        return f"<UserDetails {self.id}, {self.first_name} {self.last_name}, {self.email}, {self.phone_number}>"
+    id              = db.Column(db.Integer, primary_key=True)
+    first_name      = db.Column(db.String(40))
+    last_name       = db.Column(db.String(40))
+    email           = db.Column(db.String(40), unique=True)
+    job_title       = db.Column(db.String(60))
+    phone_number    = db.Column(db.String(14))
+    address         = db.Column(db.String(50))
+    url_image       = db.Column(db.Text)
+    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self): return f" {self.first_name}, {self.last_name}, {self.email},{self.phone_number}"
    
 class Lead(db.Model):
     __tablename__ = 'lead'
-    id = db.Column(db.Integer, primary_key=True)
-    lead_details = db.Column(db.Integer, db.ForeignKey('lead_details.id'), nullable=False)
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    lead_details_rel = db.relationship('LeadDetails', backref='lead', uselist=False)
 
-    def __repr__(self):
-        return f"<Lead {self.id}, {self.first_name}, {self.lead_details}>"
+    id              = db.Column(db.Integer, primary_key=True)
+    lead_details    = db.Column(db.Integer, db.ForeignKey('lead_details.id'))
+    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
 
 class LeadDetails(db.Model):
     __tablename__ = 'lead_details'
@@ -58,97 +47,80 @@ class LeadDetails(db.Model):
     url_image       = db.Column(db.Text)
     comments        = db.Column(db.Text)
     datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self): return f"{self.first_name} | {self.email} | {self.organization}"
     
 class Activity(db.Model): 
     __tablename__ = 'activity'
-    id = db.Column(db.Integer, primary_key=True)
-    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'), nullable=False)
-    activity_details_id = db.Column(db.Integer, db.ForeignKey('activity_details.id'), nullable=False)
-    lead = db.relationship('Lead', backref='activities')
-    activity_details = db.relationship('ActivityDetails', backref='activities')
 
-    def __repr__(self):
-        return f"<Activity {self.id}, {self.lead_id}, {self.activity_details_id}>"
+    id                  = db.Column(db.Integer, primary_key=True)
+    lead_id             = db.Column(db.Integer, db.ForeignKey('lead.id'))
+    activity_details_id = db.Column(db.Integer, db.ForeignKey('activity_details.id'))
+    lead                = db.relationship('Lead', backref='activities')
+    activity_details    = db.relationship('ActivityDetails', backref='activities')
 
 class ActivityDetails(db.Model):
-    __tablename__ = 'activity_details'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40))
-    activity_type = db.Column(db.String(40))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    user = db.relationship('User', backref='activity_details')
+    __tablename__   = 'activity_details'
 
-    def __repr__(self):
-        return f"<ActivityDetails {self.id}, {self.title}, {self.activity_type}>"
+    id              = db.Column(db.Integer, primary_key=True)
+    title           = db.Column(db.String(40))
+    activity_type   = db.Column(db.String(40))
+    user_id         = db.Column(db.Integer, db.ForeignKey('user.id'))
+    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Communication(db.Model):
     __tablename__ = 'communication'
-    id = db.Column(db.Integer, primary_key=True)
-    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'), nullable=False)
-    communication_details_id = db.Column(db.Integer, db.ForeignKey('communication_details.id'), nullable=False)
-    lead = db.relationship('Lead', backref='communications')
-    communication_details = db.relationship('CommunicationDetails', backref='communications')
 
-    def __repr__(self):
-        return f"<Communication {self.id}, {self.lead_id}, {self.communication_details_id}>"
+    id                       = db.Column(db.Integer, primary_key=True)
+    lead_id                  = db.Column(db.Integer, db.ForeignKey('lead.id'))
+    communication_details_id = db.Column(db.Integer, db.ForeignKey('communication_details.id'))
 
 class CommunicationDetails(db.Model):
     __tablename__ = 'communication_details'
-    id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(60))
-    text = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    user = db.relationship('User', backref='communication_details')
 
-    def __repr__(self):
-        return f"<CommunicationDetails {self.id}, {self.subject}, {self.user_id}>"
+    id              = db.Column(db.Integer, primary_key=True)
+    subject         = db.Column(db.String(60))
+    text            = db.Column(db.Text)
+    user_id         = db.Column(db.Integer, db.ForeignKey('user.id'))
+    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):return f"{self.text}"
 
 class Skill(db.Model):
     __tablename__ = 'skills'
-    id = db.Column(db.Integer, primary_key=True)
-    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'), nullable=False)
-    lead = db.relationship('Lead', backref='skills')
-    skill_details_id = db.Column(db.Integer, db.ForeignKey('skills_details.id'), nullable=False)
-    skill_details = db.relationship('SkillDetails', backref='skills')
 
-    def __repr__(self):
-        return f"<Skill {self.id}, {self.lead_id}, {self.skill_details_id}>"
+    id               = db.Column(db.Integer, primary_key=True)
+    lead_id          = db.Column(db.Integer, db.ForeignKey('lead.id'))
+    skill_details_id = db.Column(db.Integer, db.ForeignKey('skills_details.id'))
+
 
 class SkillDetails(db.Model):
     __tablename__ = 'skills_details'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __repr__(self):
-        return f"<SkillDetails {self.id}, {self.title}>"
+    id              = db.Column(db.Integer, primary_key=True)
+    title           = db.Column(db.String(60))
+    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):return f" {self.title}"
 
 class Tag(db.Model):
     __tablename__ = 'tags'
-    id = db.Column(db.Integer, primary_key=True)
-    tag_detail_id = db.Column(db.Integer, db.ForeignKey('tags_details.id'), nullable=False)
-    tag_detail = db.relationship('TagDetails', backref='tags')
-    lead_id = db.Column(db.Integer, db.ForeignKey('lead.id'), nullable=False)
-    lead = db.relationship('Lead', backref='tags')
 
-    def __repr__(self):
-        return f"<Tag {self.id}, {self.tag_detail_id}, {self.lead_id}>"
+    id            = db.Column(db.Integer, primary_key=True)
+    tag_detail_id = db.Column(db.Integer, db.ForeignKey('tags_details.id'))
+    lead_id       = db.Column(db.Integer, db.ForeignKey('lead.id'))
 
 class TagDetails(db.Model):
     __tablename__ = 'tags_details'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(60))
-    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
-    datetime_create = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
-    def __repr__(self):
-        return f"<TagDetails {self.id}, {self.title}>"
+    id              = db.Column(db.Integer, primary_key=True)
+    title           = db.Column(db.String(60))
+    datetime_update = db.Column(db.DateTime, onupdate=datetime.utcnow) 
+    datetime_create = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):return f" {self.title}"
 
