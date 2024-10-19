cc3f133ea2afb206575f3b3c7c021aa3fa103406
GoldSatan <vadim.romanyuk.2003@gmail.com>
2024-10-19 10:53:38 +0300

    vadym changes

--- docs/v1/app/app.py
+++ docs/v1/app/app.py
@@ -8,7 +8,7 @@ user = 'root'
 password = '0000'
 host = 'localhost'
 port = '3306'
-database = 'tax-crm'
+database = 'tax_crm'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
