5adf06b1a964381cf3f7346dd63535cbe12f79c2
Vladyslav <vladuslavkondrun12@gmail.com>
2024-10-20 16:48:04 +0300

    Fixed the errors

--- docs/v1/app/Checking_Database.py
+++ docs/v1/app/Checking_Database.py
@@ -1,16 +1,79 @@
 from app import app, db
 from models import *
 
-with app.app_context():
-    
-    new_user_details = UserDetails(first_name='John', last_name='Doe', email='john@email.com', job_title='Manager', phone='1234567890', address='123 Main St', url_image='https/hshygyawgds')
-    db.session.add(new_user_details)
-    
-    user = UserDetails(first_name='Jane', last_name='Smith', email='jane@email.com', job_title='Developer', phone='0987654321', address='456 Second St')
-    db.session.add(user)
-    
-    user1 = UserDetails(first_name='Obama', last_name='Bumson', email='obama@email.com', job_title='Developer', phone='0987654321', address='456 Second St')
-    db.session.add(user1)
-    
-    db.session.commit()
-    print("DATA ADED")
+def insertToAllTables():
+    try:
+        with app.app_context():
+            user_details_1 = UserDetails(firstname='John', lastname='Doe',   email='2@email.com', job_title='Manager',   phone='1234567890', addres='123 Main St')
+            user_details_2 = UserDetails(firstname='Jane', lastname='Smith', email='1@email.com', job_title='Developer', phone='0987654321', addres='456 Second St')
+
+            db.session.add(user_details_1)
+            db.session.add(user_details_2)
+            db.session.commit()
+            
+            user_1 = User(user_basic_details=user_details_1.id)
+
+            db.session.add(user_1)
+            db.session.commit()
+            
+            lead_details_1 = LeadDetails(firstname='Alice', lastname='Johnson', ORGANIZATION='Company A', email='alice@company.com', phone_number='1234567890', addres='789 Third St')
+            lead_details_2 = LeadDetails(firstname='Bob',   lastname='Smith',   ORGANIZATION='Company B', email='bob@company.com',   phone_number='0987654321', addres='123 Fourth St')
+
+            db.session.add(lead_details_1)
+            db.session.add(lead_details_2)
+            db.session.commit()
+            
+            lead_1 = Lead(firstname='Alice', lead_details=lead_details_1.id)
+            lead_2 = Lead(firstname='Bob',   lead_details=lead_details_2.id)
+
+            db.session.add(lead_1)
+            db.session.add(lead_2)
+            
+            db.session.commit()
+            
+            activity_details_1 = ActivityDetails(title='Meeting',  activity_type='Call', user=user_1.id)
+            activity_details_2 = ActivityDetails(title='Sleeping', activity_type='Rest', user=user_1.id)
+
+            db.session.add(activity_details_1)
+            db.session.add(activity_details_2)
+            db.session.commit()
+            
+            activity_1 = Activity(lead=lead_details_1.id, activity_details=activity_details_1.id)
+            activity_2 = Activity(lead=lead_details_2.id, activity_details=activity_details_2.id)
+
+            db.session.add(activity_1)
+            db.session.add(activity_2)
+            db.session.commit()
+            
+            communication_details = CommunicationDetails(subject='Follow up', text='Following up', user=user_details_1.id)
+
+            db.session.add(communication_details)
+            db.session.commit()
+            
+            communication_1 = Communication(lead=lead_1.id, communication_details=communication_details.id)
+
+            db.session.add(communication_1)
+            db.session.commit()
+            
+            skills_details = SkillDetails(title='Python')
+
+            db.session.add(skills_details)
+            db.session.commit()
+            
+            skills_1 = Skill(lead=lead_1.id, skills_details=skills_details.id)
+
+            db.session.add(skills_1)
+            db.session.commit()
+            
+            tags_details = TagDetails(title='EXPERT')
+
+            db.session.add(tags_details)
+            db.session.commit()
+
+            tags_1 = Tag(lead=lead_1.id, tags_details=tags_details.id)
+
+            db.session.add(tags_1)
+            db.session.commit()
+
+    except Exception as e:
+        print(f'Inserting data error.\nError: {e}')
--- docs/v1/app/app.py
+++ docs/v1/app/app.py
@@ -1,14 +1,10 @@
 from flask import Flask, render_template, jsonify, redirect, flash, session
 from models import *
-<<<<<<< HEAD
-=======
-from testing_db import *
->>>>>>> dd396cf805fb55f6be0cb6b763fd160755089736
+from Checking_Database import *
 
 app = Flask(__name__)
 app.secret_key = 'Asdasd@E!d121'
 
-<<<<<<< HEAD
 username='root'
 password='0000'
 host='localhost'
@@ -16,16 +12,8 @@ port='3306'
 database='tax_crm'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
-=======
-user = 'root'
-password = '0000'
-host = 'localhost'
-port = '3306'
-database = 'tax_crm'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
->>>>>>> dd396cf805fb55f6be0cb6b763fd160755089736
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
+user = 'root'
 
 db.init_app(app)
 
@@ -40,33 +28,20 @@ def base_view():
 
 @app.route('/users')
 def users_view():
-<<<<<<< HEAD
-    users = UserDetails.query.all()
-    return render_template('pages/Checking_Database.html', users=users)
-
-
-'''@app.route('/create-lead')
-def create_lead_view():
-     leads = UserDetails.query.all()
-     return render_template('pages/Checking_Database.html', leads=leads)'''
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8080, host='0.0.0.0')  
-=======
     user = User.query.all()
     lead = Lead.query.all()
     activity = Activity.query.all()
     communication = Communication.query.all()
-    skils = Skills.query.all()
-    tags = Tags.query.all()
+    skils = Skill.query.all()
+    tags = Tag.query.all()
     
-    users_details = User_details.query.all()
-    leads_details = Lead_details.query.all()
-    activities_details = Activity_details.query.all()
-    skills_details = Skills_details.query.all()
-    tags_details = Tags_details.query.all()
+    users_details = UserDetails.query.all()
+    leads_details = LeadDetails.query.all()
+    activities_details = ActivityDetails.query.all()
+    skills_details = SkillDetails.query.all()
+    tags_details = TagDetails.query.all()
 
-    return render_template('pages/database_testing.html', 
+    return render_template('pages/Checking_Database.html', 
                            user=user,
                            lead=lead,
                            activity=activity,
@@ -86,10 +61,6 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
     insertToAllTables()
 
     app.run(debug=True, port=8080, host='0.0.0.0')
-    
-    
->>>>>>> dd396cf805fb55f6be0cb6b763fd160755089736
