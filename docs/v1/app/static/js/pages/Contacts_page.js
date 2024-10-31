a48f6ae9a1eac409272cb9a4a36683e1a6216788
Vladyslav <vladuslavkondrun12@gmail.com>
2024-10-31 20:59:43 +0200

    the task is completed

--- docs/v1/app/static/js/pages/Contacts_page.js
+++ docs/v1/app/static/js/pages/Contacts_page.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const item = document.querySelector(".item");
+    const information_content = document.querySelector(".information_content");
 
+    item.forEach(item => {
+        item.addEventListener('click', () => {
+
+            item.forEach(i => i.classList.remove('active'));
+            information_content.forEach(content => {
+                content.classList.remove('active');
+                content.querySelector('.All_content').style.display = 'none';
+            });
+            item.classList.add('active');
+
+            const activeContent = document.querySelector(`#${information.dataset.active}`);
+            activeContent.classList.add('active');
+            activeContent.querySelector('.All_content').style.display = 'block';
+        });
+    });
+
+    document.querySelector('.All_content').style.display = 'block';  
