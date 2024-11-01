babd1e6d5f7fe7251aa1c2e295b6f111687a436f
Vladyslav <vladuslavkondrun12@gmail.com>
2024-11-01 13:39:51 +0200

    The task is completed

--- docs/v1/app/static/js/pages/Contacts_page.js
+++ docs/v1/app/static/js/pages/Contacts_page.js
@@ -1,21 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const item = document.querySelector(".item");
-    const information_content = document.querySelector(".information_content");
-
-    item.forEach(item => {
-        item.addEventListener('click', () => {
-
-            item.forEach(i => i.classList.remove('active'));
-            information_content.forEach(content => {
-                content.classList.remove('active');
-                content.querySelector('.All_content').style.display = 'none';
-            });
-            item.classList.add('active');
-
-            const activeContent = document.querySelector(`#${information.dataset.active}`);
-            activeContent.classList.add('active');
-            activeContent.querySelector('.All_content').style.display = 'block';
-        });
+function showTabContent(contentClass) {
+    document.querySelectorAll('.all_contact_content, .clients_content, .leads_content').forEach(function (content) {
+        content.style.display = 'none';
     });
-
-    document.querySelector('.All_content').style.display = 'block';  
+    document.querySelector(`.${contentClass}`).style.display = 'block';
+    document.querySelectorAll('.tabs > div').forEach(function (tab) {
+        tab.classList.remove('active');
+    });
+    const activeTab = [...document.querySelectorAll('.tabs > div')].find(tab => {
+        return tab.getAttribute('onclick').includes(contentClass);
+    });
+    if (activeTab) {
+        activeTab.classList.add('active');
+    }
+}
+showTabContent('all_contact_content');
