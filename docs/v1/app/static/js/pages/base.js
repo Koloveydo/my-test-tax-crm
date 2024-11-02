97bfb1c2da61fab49d2b662a383d8053fd3ef653
ArtemSlobodeniuk <artemslobodinuk1@gmail.com>
2024-11-02 11:44:42 +0200

    Header

--- docs/v1/app/static/js/pages/base.js
+++ docs/v1/app/static/js/pages/base.js
@@ -7,10 +7,39 @@ menu_button.addEventListener('click', () => {
     close_menu.classList.toggle('active')  
     phone_menu.classList.toggle('active')    
 })
-
-// Close menu
 close_menu.addEventListener('click', () => {
     menu_button.classList.toggle('active')   
     close_menu.classList.toggle('active')   
     phone_menu.classList.toggle('active')    
 })
+
+let clicks = 0;
+let clicks_2 = 0;
+
+const link_button = document.querySelector('.contacts_link');
+const link_button_2 = document.querySelector('.my_profile_link');
+const mediaQuery = window.matchMedia('(min-width: 900px) and (max-width: 1399px)');
+
+link_button.addEventListener('click', function(event) {
+    if (mediaQuery.matches) {
+        event.preventDefault();
+        clicks++;
+
+        if (clicks === 2) {
+            window.location.href = 'http://192.168.43.146:8080/';
+            clicks = 0;
+        }
+    }
+});
+link_button_2.addEventListener('click', function(event) {
+    if (mediaQuery.matches) {
+        event.preventDefault();
+        clicks_2++;
+
+        if (clicks_2 === 2) {
+            window.location.href = 'http://192.168.43.146:8080/';
+            clicks_2 = 0;
+        }
+    }
+});
+
