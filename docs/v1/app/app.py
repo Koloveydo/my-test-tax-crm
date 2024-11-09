ed2b939ebc42ae3914974e70b51d7563edf49afc
GoldSatan <vadim.romanyuk.2003@gmail.com>
2024-11-09 11:31:50 +0200

    vadym, changes styles

--- docs/v1/app/app.py
+++ docs/v1/app/app.py
@@ -8,7 +8,7 @@ user = 'root'
 password = '0000'
 host = 'localhost'
 port = '3306'
-database = 'tax-crm'
+database = 'tax_crm'
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}'
@@ -18,7 +18,7 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+
 @app.route('/components')
 def components_view():
     return render_template('pages/components.html')
@@ -37,28 +37,48 @@ def users_view():
     users = User_details.query.all()
     return render_template('pages/database_testing.html', users=users)
 
-@app.route('/index')
-def main_page_view():
+@app.route('/')
+def indexView():
 
     callendar_data = [
         {'name':'','photo':''},
         {'name2':'','photo2':''},
     ]
     user = [
-
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
     ]
     tasks = [
-
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
     ]
     unmess = [
-
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
     ]
     leads = [
-        { 'Vadim Romaniyk'}
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
+        'Vadim Romaniyk',
     ]
     
+    data = {
+        "callendar_data": callendar_data,
+        "user": user,
+        "tasks": tasks,
+        "unmess": unmess,
+        "leads": leads,
+    }
     
-    return render_template('pages/index.html', callendar_data=callendar_data, user=user, tasks=tasks, unmess=unmess, leads=leads)
+    
+    return render_template('pages/index.html', data=data)
 
 if __name__ == "__main__":
     with  app.app_context():
