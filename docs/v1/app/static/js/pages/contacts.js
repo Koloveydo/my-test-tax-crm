13ca0c93be823831aa279628fdebbf9e1d94ba8f
Vladyslav <vladuslavkondrun12@gmail.com>
2024-11-14 19:28:23 +0200

    The task is completed

--- docs/v1/app/static/js/pages/contacts.js
+++ docs/v1/app/static/js/pages/contacts.js
@@ -1,36 +1,30 @@
 const toggleActiveCol = () => {
-    const allColumnToClick  = document.querySelectorAll(".head-tab-click"); // Вкладки
-    const allDataColumn     = document.querySelectorAll(".col-content-contants"); // Колонки з даними
-    const addTextButtons    = document.querySelectorAll(".add_contact"); // Кнопки для зміни тексту
+    const allColumnToClick  = document.querySelectorAll(".head-tab-click");
+    const allDataColumn     = document.querySelectorAll(".col-content-contants");
+    const addTextButtons    = document.querySelectorAll(".add_contact"); 
 
     allColumnToClick.forEach(btn => {
         btn.addEventListener("click", function () {
-            const activeColumnToClick = document.querySelector(".head-tab-click.active"); // Поточна активна вкладка
-            const activeDataColumn = document.querySelector(".col-content-contants.active"); // Поточна активна колонка
+            const activeColumnToClick = document.querySelector(".head-tab-click.active");
+            const activeDataColumn = document.querySelector(".col-content-contants.active"); 
 
             if (btn !== activeColumnToClick) {
-                // Скидаємо клас "active" з поточної вкладки
                 activeColumnToClick.classList.remove("active");
                 btn.classList.add("active");
 
-                // Визначаємо індекс вкладки
                 const currentActiveIndex = parseInt(btn.classList[1].split("-")[1]);
 
-                // Скидаємо клас "active" з поточної колонки
                 if (activeDataColumn) {
                     activeDataColumn.classList.remove("active");
                 }
 
-                // Додаємо клас "active" до нової колонки
                 allDataColumn[currentActiveIndex].classList.add("active");
 
-                // Скидаємо клас "active" з поточної кнопки
-                const activeAddTextButton = document.querySelector(".add_contact.active"); // Поточна активна кнопка
+                const activeAddTextButton = document.querySelector(".add_contact.active"); 
                 if (activeAddTextButton) {
                     activeAddTextButton.classList.remove("active");
                 }
 
-                // Додаємо клас "active" до відповідної кнопки
                 addTextButtons[currentActiveIndex].classList.add("active");
             }
         });
