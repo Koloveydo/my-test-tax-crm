048785a11a8d4065181a29c02b50487db66c981e
Koloveydo <107243603+Koloveydo@users.noreply.github.com>
2024-11-14 21:11:42 +0200

    Complete page

--- docs/v1/app/static/js/pages/index.js
+++ docs/v1/app/static/js/pages/index.js
@@ -7,20 +7,29 @@ document.getElementById("datetime").value = datetime;
 
 function updateGreetings(){
 	const inputElement = document.getElementById("greetings_value")
+	const inputElements = document.getElementById("greetings_values")
 	const now = new Date();
 	const hours = now.getHours();
 	let greetingsText;
+	let greetingsTexts;
 
 	if (hours >= 5 && hours < 12) {
-		greetingsText = "Good morning, have a nice day!";
-	} else if (hours >= 12 && hours < 18){
-		greetingsText = "Good afternoon, you're doing great work!";
+		greetingsText = "Good morning,";
+		greetingsTexts = "have a nice day!";
+		document.getElementById("greetings_values").style.display = "block";
+	} else if (hours >= 22 && hours < 0){
+		greetingsText = "Good afternoon,";
+		greetingsTexts = "you're doing great work!";
+		document.getElementById("greetings_values").style.display = "block";
 	} else if (hours >= 18 && hours < 22){
-		greetingsText = "Good evening, you're very diligent!";
+		greetingsText = "Good evening,";
+		greetingsTexts = "you're very diligent!";
+		document.getElementById("greetings_values").style.display = "block";
 	} else {
 		greetingsText = "Good night, my master!";
 	}
 
 	inputElement.value = greetingsText;
+	inputElements.value = greetingsTexts;
 }
 document.addEventListener('DOMContentLoaded', updateGreetings);
