a3fa36a6998a6eed7af39764527b2e686ea04c89
Koloveydo <107243603+Koloveydo@users.noreply.github.com>
2025-04-22 00:09:32 +0300

    added some changes, added setting page as ajax responce, changed some issues, changed login page

--- docs/v1/app/static/js/pages/base.js
+++ docs/v1/app/static/js/pages/base.js
@@ -8,3 +8,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+/* ----- open setting popup ------ */
+
+async function openSettings() {
+    const response = await fetch("/setting");
+    const html = await response.text();
+    document.body.insertAdjacentHTML("beforeend", html);
+}
+
+document.addEventListener("click", (e) => {
+    const elSetting = e.target.closest("[data_open_settings]");
+    if (elSetting) {
+        e.preventDefault();
+        openSettings();
+    }
+});
+
+/* ------- close setting popup --------- */
+
+document.addEventListener("click", (e) => {
+    if (e.target.closest(".close_setting_svg")) {
+        const modal = document.getElementById("setting-popup");
+        if (modal) {
+            modal.remove();
+        }
+    }
+});
